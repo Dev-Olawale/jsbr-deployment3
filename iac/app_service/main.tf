f13c78dc1d852e474478e19d780bfdc5c6b226f5
Abdulwahab Abdulliameed <39828516+wahabajasco@users.noreply.github.com>
2023-03-01 11:01:20 +0100

    Update main.tf

--- iac/app_service/main.tf
+++ iac/app_service/main.tf
@@ -1,18 +1,3 @@
-# Create Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# Create Container Registry
-resource "azurerm_container_registry" "acr" {
-  name                = "jsbrcr"
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  sku                 = "Standard"
-  admin_enabled       = true
-}
-
 # Create App Service Plan
 resource "azurerm_service_plan" "asp" {
   name                = "jsbr-deployment-appServiceplan"
@@ -35,4 +20,28 @@ resource "azurerm_linux_web_app" "lwapp" {
     always_on        = true
     linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/<docker-image-name>:$(tag)"
   }
+}
+
+# Create Container Registry Webhook
+resource "azurerm_container_registry_webhook" "webhooks" {
+  name                = "jsbrdeploymentwebAppHook"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  registry_name       = "jsbrcr"
+  service_uri         = "https://${azurerm_linux_web_app.lwapp.name}.azurewebsites.net/docker/hook"
+  actions             = ["push"]
+  scope               = "azurerm_container_registry.acr.name:$(tag)"
+  status              = "enabled"
+  custom_headers = {
+    "X-Custom-Header" = "CustomValue"
+  }
+}
+
+# Monitoring - app insight
+resource "azurerm_application_insights" "insight" {
+  name                = "jsbr-insight"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_type    = "java"
+  retention_in_days   = 30
 }
