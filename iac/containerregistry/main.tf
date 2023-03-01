a9e31b35b6c552d540af36ba332b0f0b186dfa97
Abdulwahab Abdulliameed <39828516+wahabajasco@users.noreply.github.com>
2023-03-01 11:02:01 +0100

    Update main.tf

--- iac/containerregistry/main.tf
+++ iac/containerregistry/main.tf
@@ -12,27 +12,3 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Standard"
   admin_enabled       = true
 }
-
-# Create App Service Plan
-resource "azurerm_service_plan" "asp" {
-  name                = "jsbr-deployment-appServiceplan"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku_name            = "S1"
-  os_type             = "Linux"
-}
-
-# Create App Service for Linux Container
-resource "azurerm_linux_web_app" "lwapp" {
-  name                       = "jsbr-deployment-webApp"
-  location                   = var.location
-  resource_group_name        = var.resource_group_name
-  service_plan_id            = azurerm_service_plan.asp.id
-  https_only                 = true
-  client_certificate_enabled = true
-
-  site_config {
-    always_on        = true
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/<docker-image-name>:$(tag)"
-  }
-}
