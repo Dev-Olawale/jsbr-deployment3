a179e78cce00cf4c7787595a237b0602c063ab0d
Abdulwahab Abdulliameed <39828516+wahabajasco@users.noreply.github.com>
2023-03-01 12:27:52 +0100

    Update variables.tf

--- iac/variables.tf
+++ iac/variables.tf
@@ -5,13 +5,26 @@ variable "resource_group_name" {
 }
 
 variable "location" {
+  description = "The Azure Region were the resource should exist"
   default = "westeurope"
 }
 
 variable "acr_name" {
+  description = "The Azure Container Registry for the java react app deployment Docker image"
   default = "jsbrcr"
 }
 
-variable "azurerm_service_plan_name" {
+variable "asp_name" {
+  description = "The app service plan for the java react app deployment webApp"
   default = "jsbr-deployment-appServiceplan"
+}
+
+variable "webhooks_name" {
+  description = "The Webhook for the container registry created for the java react app deployment"
+  default = "jsbrdeploymentwebAppHook"
+}
+
+variable "insight_name" {
+  description = "The Azure monitor insight"
+  default = "jsbr-insight"
 }
