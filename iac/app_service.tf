18746c642cc2aed6cdeaae8dfa95a35fed4d514e
Abdulwahab Abdulliameed <39828516+wahabajasco@users.noreply.github.com>
2023-03-01 12:33:19 +0100

    Update app_service.tf

--- iac/app_service.tf
+++ iac/app_service.tf
@@ -11,7 +11,7 @@ resource "azurerm_service_plan" "asp" {
 
 # Create App Service for Linux Container
 resource "azurerm_linux_web_app" "lwapp" {
-  name                       = "jsbr-deployment-webApp"
+  name                       = var.lwapp_name
   location                   = var.location
   resource_group_name        = var.resource_group_name
   service_plan_id            = azurerm_service_plan.asp.id
@@ -20,17 +20,17 @@ resource "azurerm_linux_web_app" "lwapp" {
 
   site_config {
     always_on        = true
-    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/<docker-image-name>:$(tag)"
+    #linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/<docker-image-name>:$(tag)"
   }
 }
 
 # Create Container Registry Webhook
 resource "azurerm_container_registry_webhook" "webhooks" {
-  name                = "jsbrdeploymentwebAppHook"
+  name                = var.webhooks_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  registry_name       = "jsbrcr"
-  service_uri         = "https://${azurerm_linux_web_app.lwapp.name}.azurewebsites.net/docker/hook"
+  registry_name       = var.acr_name
+  #service_uri         = "https://${azurerm_linux_web_app.lwapp.name}.azurewebsites.net/docker/hook"
   actions             = ["push"]
   scope               = "azurerm_container_registry.acr.name:$(tag)"
   status              = "enabled"
@@ -41,7 +41,7 @@ resource "azurerm_container_registry_webhook" "webhooks" {
 
 # Monitoring - app insight
 resource "azurerm_application_insights" "insight" {
-  name                = "jsbr-insight"
+  name                = var.insight_name
   location            = var.location
   resource_group_name = var.resource_group_name
   application_type    = "java"
