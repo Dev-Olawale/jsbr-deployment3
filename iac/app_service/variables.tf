547b1ee1640c7cd57b24abdf01d61ec305473868
Abdulwahab Abdulliameed <39828516+wahabajasco@users.noreply.github.com>
2023-03-01 10:29:48 +0100

    Create variables.tf

---
+++ iac/app_service/variables.tf
@@ -0,0 +1,12 @@
+# Define variables for resource names and locations
+variable "resource_group_name" {
+  default = "jsbr-RG"
+}
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "acr_name" {
+  default = "jsbrcr"
+}
