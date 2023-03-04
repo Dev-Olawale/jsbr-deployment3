6551c86bde47da147b80be5bd67e842475f9ce71
Abdulwahab Abdulliameed <39828516+wahabajasco@users.noreply.github.com>
2023-03-04 21:53:14 +0100

    Update app_service.tf

--- iac/app_service.tf
+++ iac/app_service.tf
@@ -11,31 +11,14 @@ resource "azurerm_service_plan" "asp" {
 
 # Create App Service for Linux Container
 resource "azurerm_linux_web_app" "lwapp" {
-  name                       = var.lwapp_name
+  name                       = var.web_app_name
   location                   = var.location
   resource_group_name        = var.resource_group_name
   service_plan_id            = azurerm_service_plan.asp.id
-  #https_only                 = true
-  #client_certificate_enabled = true
 
   site_config {
     #always_on        = true
     #linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/<docker-image-name>:$(tag)"
-  }
-}
-
-# Create Container Registry Webhook
-resource "azurerm_container_registry_webhook" "webhooks" {
-  name                = var.webhooks_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  registry_name       = var.acr_name
-  service_uri         = "https://${azurerm_linux_web_app.lwapp.name}.azurewebsites.net/docker/hook"
-  actions             = ["push"]
-  scope               = "azurerm_container_registry.acr.name:latest"
-  status              = "enabled"
-  custom_headers = {
-    "X-Custom-Header" = "CustomValue"
   }
 }
 
