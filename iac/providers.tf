152e1b0db3b4420a81933fdddeb22a36d448f568
Abdulwahab Abdulliameed <39828516+wahabajasco@users.noreply.github.com>
2023-03-05 22:03:24 +0100

    Update providers.tf

--- iac/providers.tf
+++ iac/providers.tf
@@ -8,10 +8,6 @@ terraform {
   }
   
   backend "azurerm" {
-    resource_group_name  = "terraform-RG"
-    storage_account_name = "jsbrtorageaccounts"
-    container_name       = "jsbrcontainer"
-    key                  = "jsbrterraform.tfstate"
   }
 
   required_version = ">= 1.1.0"
@@ -19,9 +15,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
